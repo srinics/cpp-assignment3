5eb8521c45e25817a2c7de4daf5ee81fbf4f957f
srinics <srinics@hotmail.com>
2020-03-26 11:34:34 +0530

    Updated CompileLinux.sh

--- CompileLinux.sh
+++ CompileLinux.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 op="$1"
-binary_name="linux-out"
-binary_name2="linux-proc"
+binary_name="linux-si"
+binary_name2="linux-pl"
 if [ "$op" = "clean" ]; then
 	rm -rf $binary_name
 	rm -rf $binary_name2
 	exit 0;
 fi
-g++ -g Main.cpp -o $binary_name -lpthread -lrt
+g++ -g SingleInstanceApp.cpp -o $binary_name -lpthread -lrt
 g++ -g ProcessList.cpp -o $binary_name2 -lpthread -lrt
